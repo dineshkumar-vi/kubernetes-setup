30b83df5eafa7debb7b5cdb4871c6f07b472a78a
Dinesh Kumar <dineshkumar.viveckanandan@photoninfotech.net>
2019-12-27 01:10:40 -0600

    Basci version of service and relevant k8 configs

---
+++ src/main/java/com/kubernetes/setup/demo/controller/DemoController.java
@@ -0,0 +1,18 @@
+package com.kubernetes.setup.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class DemoController {
+
+    @GetMapping
+    public ResponseEntity<String> userData() throws Exception{
+        return new ResponseEntity("Sample User", HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/com/kubernetes/setup/demo/controller/HealthController.java
@@ -0,0 +1,18 @@
+package com.kubernetes.setup.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping
+    public ResponseEntity healthCheck() throws Exception {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+}
